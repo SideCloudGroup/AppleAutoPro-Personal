ca16ceb0049c6544344d9be5b7b6c7089c2d0fbd
pplulee <pplulee@live.cn>
2025-01-29 11:54:59 +0000

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -10,16 +10,43 @@ repo="SideCloudGroup/AppleAutoPro-Personal"
 filename="AppleAutoPro-Personal"
 LATEST_TAG=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
+geo_check() {
+    api_list="https://blog.cloudflare.com/cdn-cgi/trace https://dash.cloudflare.com/cdn-cgi/trace https://developers.cloudflare.com/cdn-cgi/trace"
+    ua="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"
+    set -- "$api_list"
+    for url in $api_list; do
+        text="$(curl -A "$ua" -m 10 -s "$url")"
+        endpoint="$(echo "$text" | sed -n 's/.*h=\([^ ]*\).*/\1/p')"
+        if echo "$text" | grep -qw 'CN'; then
+            isCN=true
+            break
+        elif echo "$url" | grep -q "$endpoint"; then
+            break
+        fi
+    done
+}
+
+geo_check
+
 export COMPOSER_ALLOW_SUPERUSER=1
 if [ ! -e "think" ]; then
     echo -e "${RED}请在网站根目录执行该脚本！${NC}"
     exit;
 fi
 
+PHP_VERSION=$(php -v | grep -oP '^PHP \K[0-9]+\.[0-9]+')
+REQUIRED_PHP_VERSION="7.4"
+
+if [ "$PHP_VERSION" != "$REQUIRED_PHP_VERSION" ]; then
+    echo -e "${RED}PHP版本不是$REQUIRED_PHP_VERSION，已退出${NC}"
+    exit 1
+fi
+
 if ! command -v unzip &> /dev/null || ! command -v curl &> /dev/null || ! command -v wget &> /dev/null || ! command -v jq &> /dev/null || ! command -v rsync &> /dev/null; then
     echo -e "${YELLOW}缺少必要的工具，正在安装……${NC}"
     if [ -f /etc/debian_version ]; then
-        apt update && apt -y install unzip curl wget jq rsync
+        apt update
+        apt -y install unzip curl wget jq rsync
     elif [ -f /etc/redhat-release ]; then
         yum -y install unzip curl wget jq rsync
     else
@@ -33,11 +60,13 @@ echo -e "${BLUE}If there are changes in the file, it will be replaced with the l
 echo -e "${YELLOW}请按回车继续执行更新 | Press enter to continue...${NC}"
 read
 echo -e "${GREEN}正在升级到最新版本：$LATEST_TAG${NC}"
-if [[ $(curl -m 10 -s https://ipapi.co/json | grep 'China') != "" ]]; then
-    wget -T 20 -q "https://kkgithub.com/$repo/releases/download/$LATEST_TAG/$filename.zip" -O "$filename.zip"
+
+if [ -n "$isCN" ]; then
+    wget -T 20 -q "https://ghfast.top/github.com/$repo/releases/download/$LATEST_TAG/$filename.zip" -O "$filename.zip"
 else
     wget -T 20 -q "https://github.com/$repo/releases/download/$LATEST_TAG/$filename.zip" -O "$filename.zip"
 fi
+
 if [ $? -ne 0 ]; then
     echo -e "${RED}wget失败或超时，退出程序${NC}"
     exit 1
@@ -54,7 +83,11 @@ cp ./public/favicon.ico "$filename/public"
 rsync -aq --remove-source-files "$filename/" ./
 rm -rf "$filename"
 rm -rf "$filename.zip"
-UPGRADE_DB=$(jq -r '.upgrade_db' "$UPGRADE_FILE")
+
+if [ -n "$isCN" ]; then
+  php composer.phar config repo.packagist composer https://mirrors.aliyun.com/composer/
+fi
+
 php think migrate:run
 php composer.phar upgrade --no-interaction
 chmod -R 755 ./*
