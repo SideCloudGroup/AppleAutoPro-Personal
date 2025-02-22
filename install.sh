37bca31519cb9b2e2c1a1902ab01a422b7784579
pplulee <hi@pplulee.me>
2025-02-22 10:55:42 +0000

    更新配置文件

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# TronPower 安装脚本
+# AppleAutoPro-Personal 安装脚本
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -7,7 +7,18 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 repo="SideCloudGroup/AppleAutoPro-Personal"
 filename="AppleAutoPro-Personal"
-
+if ! command -v unzip &> /dev/null || ! command -v curl &> /dev/null || ! command -v wget &> /dev/null || ! command -v rsync &> /dev/null; then
+    echo -e "${YELLOW}缺少必要的工具，正在安装……${NC}"
+    if [ -f /etc/debian_version ]; then
+        apt update
+        apt -y install unzip curl wget jq rsync
+    elif [ -f /etc/redhat-release ]; then
+        yum -y install unzip curl wget jq rsync
+    else
+       echo -e "${RED}无法检测到当前系统，已退出${NC}"
+       exit;
+    fi
+fi
 geo_check() {
     api_list="https://blog.cloudflare.com/cdn-cgi/trace https://dash.cloudflare.com/cdn-cgi/trace https://developers.cloudflare.com/cdn-cgi/trace"
     ua="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"
@@ -23,25 +34,21 @@ geo_check() {
         fi
     done
 }
-
-# is cn
+geo_check
 if [ -n "$isCN" ]; then
     LATEST_TAG=$(curl -s "https://ghfast.top/api.github.com/repos/$repo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 else
     LATEST_TAG=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 fi
-
 echo -e "${YELLOW}请输入安装路径（回车默认安装到/opt/AppleAutoPro-Personal）:${NC}"
 read install_path
 install_path=${install_path:-/opt/AppleAutoPro-Personal}
-
 if [ -d "$install_path" ]; then
     echo -e "${RED}$install_path 文件夹已存在，退出脚本${NC}"
     exit 1
 fi
-
+mkdir -p $install_path
 echo -e "${BLUE}检查并安装必要的软件包...${NC}"
-
 if docker >/dev/null 2>&1; then
     echo -e "${GREEN}Docker已安装${NC}"
 else
@@ -58,16 +65,6 @@ if ! docker >/dev/null 2>&1; then
     echo -e "${RED}Docker安装失败，请检查错误信息${NC}"
     exit 1
 fi
-if ! unzip -v >/dev/null 2>&1; then
-    echo -e "${YELLOW}unzip未安装，开始安装……${NC}"
-    apt-get update && apt-get install -y unzip
-    echo -e "${GREEN}unzip安装完成${NC}"
-fi
-if ! wget --version >/dev/null 2>&1; then
-    echo -e "${YELLOW}wget未安装，开始安装……${NC}"
-    apt-get update && apt-get install -y wget
-    echo -e "${GREEN}wget安装完成${NC}"
-fi
 cd $install_path
 if [ -n "$isCN" ]; then
     wget -T 20 -q "https://ghfast.top/github.com/$repo/archive/refs/heads/v4.zip" -O "v4.zip"
@@ -81,9 +78,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 unzip -q -o "v4.zip"
-mv "$filename-v4"/* ./
-rm -rf "$filename-v4"
-rm -rf "$filename"
+rsync -av --remove-source-files "$filename-4"/ ./
+rm -rf "$filename-4"
+rm -rf "v4.zip"
 unzip -q -o "$filename.zip"
 if [ ! -d "$filename" ]; then
     echo -e "${RED}$filename 目录不存在，退出更新……${NC}"
@@ -92,6 +89,6 @@ fi
 mv "$filename" web
 rm -rf "$filename.zip"
 docker compose pull
-mv .example.env .env
+chmod +x ./data/entrypoint.sh
 echo -e "${GREEN}下载完成！请继续按照教程完成接下来的步骤${NC}"
 exit 0
--- upgrade.sh
+++ upgrade.sh
@@ -7,7 +7,18 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 repo="SideCloudGroup/AppleAutoPro-Personal"
 filename="AppleAutoPro-Personal"
-
+if ! command -v unzip &> /dev/null || ! command -v curl &> /dev/null || ! command -v wget &> /dev/null || ! command -v rsync &> /dev/null; then
+    echo -e "${YELLOW}缺少必要的工具，正在安装……${NC}"
+    if [ -f /etc/debian_version ]; then
+        apt update
+        apt -y install unzip curl wget jq rsync
+    elif [ -f /etc/redhat-release ]; then
+        yum -y install unzip curl wget jq rsync
+    else
+       echo -e "${RED}无法检测到当前系统，已退出${NC}"
+       exit;
+    fi
+fi
 geo_check() {
     api_list="https://blog.cloudflare.com/cdn-cgi/trace https://dash.cloudflare.com/cdn-cgi/trace https://developers.cloudflare.com/cdn-cgi/trace"
     ua="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"
@@ -23,21 +34,17 @@ geo_check() {
         fi
     done
 }
-
 geo_check
-
 if [ -n "$isCN" ]; then
     LATEST_TAG=$(curl -s "https://ghfast.top/api.github.com/repos/$repo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 else
     LATEST_TAG=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 fi
-
 echo -e "${BLUE}如文件存在改动，一键更新后将会被替换至最新版本，改动将会消失，请注意备份${NC}"
 echo -e "${BLUE}If there are changes in the file, it will be replaced with the latest version after one-click update, and the changes will disappear. Please backup.${NC}"
 echo -e "${YELLOW}请按回车继续执行更新 | Press enter to continue...${NC}"
 read
 echo -e "${GREEN}正在升级到最新版本：$LATEST_TAG${NC}"
-
 docker compose down
 if [ -n "$isCN" ]; then
     wget -T 20 -q "https://ghfast.top/github.com/$repo/archive/refs/heads/v4.zip" -O "v4.zip"
@@ -51,23 +58,23 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 unzip -q -o "v4.zip"
-mv "$filename-v4"/* ./
-rm -rf "$filename-v4"
-rm -rf "$filename"
+rsync -av --remove-source-files "$filename-4"/ ./
+rm -rf "$filename-4"
+rm -rf "v4.zip"
 unzip -q -o "$filename.zip"
 if [ ! -d "$filename" ]; then
     echo -e "${RED}$filename 目录不存在，退出更新……${NC}"
     exit 1
 fi
-mv "$filename" web
+rm -rf ./web/app
+rsync -aq --remove-source-files "$filename/" ./web/
+rm -rf "$filename"
 rm -rf "$filename.zip"
 docker compose pull
-
+chmod +x ./data/entrypoint.sh
 echo -e "${GREEN}更新完成，请查看更新日志，检查前端配置文件是否需要改动。${NC}"
-
 docker compose pull
 docker compose up -d
-
 echo -e "${YELLOW}是否要清理旧镜像？(y/n)${NC}"
 read prune_choice
 if [ "$prune_choice" = "y" ]; then
@@ -76,6 +83,5 @@ if [ "$prune_choice" = "y" ]; then
 else
     echo -e "${YELLOW}跳过旧镜像清理${NC}"
 fi
-
 echo -e "${GREEN}升级完成！${NC}"
 exit 0
