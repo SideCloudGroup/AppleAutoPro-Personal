5be7ea78a71c7c93e1bfc29d8d8da73b724ba9c9
pplulee <hi@pplulee.me>
2025-11-20 23:42:23 +0800

    修复地区检测在alpine下无法运行

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -3,15 +3,10 @@ set -e
 mkdir -p /root/.cache/crontab
 CRON_JOB="* * * * * /usr/local/bin/php /var/www/html/think cronJob > /dev/null 2>&1"
 geo_check() {
-    api_list=(
-        "https://cloudflare.com/cdn-cgi/trace"
-        "https://blog.cloudflare.com/cdn-cgi/trace"
-        "https://dash.cloudflare.com/cdn-cgi/trace"
-        "https://developers.cloudflare.com/cdn-cgi/trace"
-    )
+    api_list="https://cloudflare.com/cdn-cgi/trace https://blog.cloudflare.com/cdn-cgi/trace https://dash.cloudflare.com/cdn-cgi/trace https://developers.cloudflare.com/cdn-cgi/trace"
     ua="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"
     isCN="false"
-    for url in "${api_list[@]}"; do
+    for url in $api_list; do
         text="$(curl -A "$ua" -m 10 -sSL "$url" 2>/dev/null)" || continue
         [ -z "$text" ] && continue
         location="$(echo "$text" | grep '^loc=' | cut -d'=' -f2)"
